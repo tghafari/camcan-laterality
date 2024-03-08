862e01293cdb3c852e923423f1d6a878391447b0
tghafari <tara.ghafari@gmail.com>
2024-03-08 12:28:09 +0000

    final touches

--- mne_python/correlations/CS03_sensorwise_correlating_lateralised_spectra_substr.py
+++ mne_python/correlations/CS03_sensorwise_correlating_lateralised_spectra_substr.py
@@ -105,7 +105,7 @@ for i, row in sensors_layout_names_df.iterrows():
                                     row["right_sensors"][0:8], 
                                     substr_lat_df)
     
-    output_corr_dir = op.join(deriv_dir, 'correlations', 'sensor_pairs_0803',
+    output_corr_dir = op.join(deriv_dir, 'correlations', 'sensor_pairs_0803_final',
                                f'{row["left_sensors"][0:8]}_{row["right_sensors"][0:8]}')
     if not op.exists(output_corr_dir):
         os.makedirs(output_corr_dir)
