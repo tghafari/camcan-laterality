b9880b863063f97064c3537867e0091fbcffedf5
tghafari <tara.ghafari@gmail.com>
2024-04-02 15:09:14 +0100

    save figures and moved and renamed spectra plotting script

--- mne_python/sensor/spectrum/S02_left_right_spectra_per_sub.py
+++ mne_python/sensor/spectrum/S02_left_right_spectra_per_sub.py
@@ -59,7 +59,7 @@ def calculate_spectral_power(epochs, n_fft, fmin, fmax):
 
     return epochspectrum
 
-def pick_sensor_pairs_epochspectrum(epochspectrum, right_sensor, left_sensor):
+def pick_sensor_pairs_epochspectrum(epochspectrum, right_sensor, left_sensor, output_dir, subjectID):
     """this code will pick sensor pairs for calculating lateralisation 
         from epochspectrum (output of previous function).
         the shape of psd is (1, 1, 119) = #epochs, #sensors, #freqs
@@ -79,8 +79,10 @@ def pick_sensor_pairs_epochspectrum(epochspectrum, right_sensor, left_sensor):
     psd_left_plot = epochspectrum.copy().pick(picks=left_sensor).plot(axes=axes[0])
     axes[0].set_title(f'Left sensor {left_sensor}')
 
+    fig.suptitle(f'Single sensor spectra for {subjectID}')
     plt.tight_layout()
-    plt.show()
+    fig.savefig(op.join(output_dir, f'sub_{subjectID}_{right_sensor}_{left_sensor}.png'))
+    plt.close()
 
     return psd_right_sensor, psd_left_sensor, freqs
 
@@ -107,23 +109,12 @@ elif platform == 'mac':
     jenseno_dir = '/Volumes/jenseno-avtemporal-attention'
 
 epoched_dir = op.join(rds_dir, 'derivatives/meg/sensor/epoched-7min50')
-info_dir = op.join(rds_dir, 'dataman/data_information')
-good_sub_sheet = op.join(info_dir, 'demographics_goodPreproc_subjects.csv')
-sensors_layout_sheet = op.join(info_dir, 'sensors_layout_names.csv')  #sensor_layout_name_grad_no_central.csv
-output_dir = op.join(rds_dir, 'derivatives/meg/sensor/lateralized_index/all_sensors_all_subs_all_freqs')
-
-# Read only data from subjects with good preprocessed data
-good_subject_pd = pd.read_csv(good_sub_sheet)
-good_subject_pd = good_subject_pd.set_index('Unnamed: 0')  # set subject id codes as the index
-
-# Read sensor layout sheet
-sensors_layout_names_df = pd.read_csv(sensors_layout_sheet)
+output_dir = op.join(jenseno_dir, 'Projects/subcortical-structures/resting-state/results/CamCan/Results/test_plots')
 
 # Preallocate lists
-subjectIDs_to_plot = [321506, 620935]
-sensor_pairs_to_plot = [['MEG0422', 'MEG1112'],['MEG0233','MEG1343']]
-sub_IDs = []
-spec_lateralisation_all_sens_all_subs = []
+subjectIDs_to_plot = [321506, 620935, 721704]  # manually add subjects to plot
+sensor_pairs_to_plot = [['MEG0422', 'MEG1112'],['MEG0233','MEG1343']]  # first is left sensor, second is right sensor in each pair
+
 
 for subjectID in subjectIDs_to_plot:
     # Read subjects one by one and calculate lateralisation index for each pair of sensor and all freqs
@@ -138,46 +129,30 @@ for subjectID in subjectIDs_to_plot:
         epochspectrum = calculate_spectral_power(epochs, n_fft=500, fmin=1, fmax=60)   # changed n_fft to 2*info['sfreq'] which after preprocessing is 250 (not 1000Hz)
 
          # Read sensor pairs and calculate lateralisation for each
-        for i in range(0, len(sensor_pairs_to_plot), 2):
+        for i in range(0, len(sensor_pairs_to_plot)):
+             print(f'plotting {sensor_pairs_to_plot[i]}')
+
              working_pair = sensor_pairs_to_plot[i]
 
              psd_right_sensor, psd_left_sensor, freqs = pick_sensor_pairs_epochspectrum(epochspectrum, 
-                                                                                           working_pair[1], 
-                                                                                           working_pair[0])     
-             spectrum_lat_sensor_pairs = calculate_spectrum_lateralisation(psd_right_sensor, psd_left_sensor)
+                                                                                        working_pair[1], 
+                                                                                        working_pair[0],
+                                                                                        output_dir,
+                                                                                        subjectID)     
+             spectrum_lat_sensor_pairs = calculate_spectrum_lateralisation(psd_right_sensor, 
+                                                                           psd_left_sensor)
              
              # Plot spectrum lateralisation of these sensors vs. frequency
-             _, ax = plt.subplots()
+             fig, ax = plt.subplots(figsize=(7, 5))
              ax.plot(freqs, spectrum_lat_sensor_pairs)
-             ax.set(title=f"lateralisation spectrum for {working_pair[1]}_{working_pair[0]}",
-                    xlabel="Frequency (Hz)",
-                    ylabel="Lateralisation Index",
-                    )
+             ax.set(title=f"lateralisation spectrum for {subjectID} in {working_pair[0]}_{working_pair[1]}",
+             xlabel="Frequency (Hz)",
+             ylabel="Lateralisation Index",
+             )
+
+             fig.savefig(op.join(output_dir, f'sub_{subjectID}_{working_pair[1]}_{working_pair[0]}_lateralisation_spectra.png'))
+             plt.close()
 
     except:
         print(f'an error occured while reading subject # {subjectID} - moving on to next subject')
         pass
-
-# Sanity check with plot_topos
-to_tests = np.arange(0,6)
-to_test_output_dir = op.join(jenseno_dir, 'Projects/subcortical-structures/resting-state/results/CamCan/Results/PSD_plot_topos')
-
-for _ in to_tests:
-    random_index = np.random.randint(0, len(good_subject_pd))
-    # Get the subject ID at the random index
-    subjectID = good_subject_pd.iloc[random_index]['SubjectID'][2:]
-    epoched_fname = 'sub-CC' + str(subjectID) + '_ses-rest_task-rest_megtransdef_epo.fif'
-    epoched_fif = op.join(epoched_dir, epoched_fname)
-    epochs = mne.read_epochs(epoched_fif, preload=True, verbose=True)  # one 7min50sec epochs
-    epochspectrum = calculate_spectral_power(epochs, n_fft=500, fmin=1, fmax=100)   
-
-    # Plot the EpochSpectrum
-    # fig = epochspectrum.plot_topo(color='k', fig_facecolor='w', axis_facecolor='w', show=False)  # raising a size error for no reason?
-    # plt.title(f'Sub_{subjectID}', y=0.9)
-    # fig.savefig(op.join(to_test_output_dir, f'sub_{subjectID}_epochspectrum_topo.png'))
-
-    # Plot a couple sensors
-    fig_sens = epochspectrum.plot()
-    plt.title(f'sub_{subjectID}')
-    fig_sens.savefig(op.join(to_test_output_dir, f'sub_{subjectID}_epochspectrum_psd.png'))
-
