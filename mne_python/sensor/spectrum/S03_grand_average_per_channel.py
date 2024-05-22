94d0f0030ddcf7ff18d42f4a0ca15960ee04bb85
tghafari <tara.ghafari@gmail.com>
2024-05-22 18:11:56 +0100

    plotting psd as evoked for plot_topo, right on left

--- mne_python/sensor/spectrum/S03_grand_average_per_channel.py
+++ mne_python/sensor/spectrum/S03_grand_average_per_channel.py
@@ -42,8 +42,7 @@ import mne
 import sys 
 import matplotlib.pyplot as plt
 
-platform = 'mac'  # are you running on bluebear or windows or mac?
-test_plot = False  # do you want sanity check plots?
+platform = 'bluebear'  # are you running on bluebear or windows or mac?
 
 def calculate_spectral_power(epochs, n_fft, fmin, fmax):
     """
@@ -69,34 +68,6 @@ def calculate_spectral_power(epochs, n_fft, fmin, fmax):
 
     return epochspectrum, epochspectrum_arr, freqs
 
-# Custom function to plot both evoked objects together
-def plot_evoked_comparison(evoked_right, evoked_left, freqs, right_color='orange', left_color='blue'):
-    # Plot right sensors
-    fig = evoked_right.plot_topo(color=right_color, show=True, title='Grand Average PSD Right vs Left Sensors')
-    
-    # Extract the axes from the figure
-    ax = fig.get_axes()
-    
-    # Plot left sensors on the same axes
-    for i, ax_i in enumerate(ax):
-        evoked_left.plot_topo(axes=ax_i, color=left_color, show=True)
-
-    # Adding a custom legend
-    handles = [plt.Line2D([0], [0], color=right_color, lw=2),
-               plt.Line2D([0], [0], color=left_color, lw=2)]
-    labels = ['Right Sensors', 'Left Sensors']
-    fig.legend(handles, labels, loc='upper right')
-
-    # Customize x-axis
-    for ax_i in ax:
-        ax_i.set_xlabel('Frequency (Hz)')
-        ax_i.set_xlim(freqs[0], freqs[-1])
-        ax_i.grid(False)  # Remove grid lines
-        ax_i.axvline(0, color='black', linewidth=1)  # Add vertical line at 0
-
-    plt.show()
-
-
 # Define where to read and write the data
 if platform == 'bluebear':
     rds_dir = '/rds/projects/q/quinna-camcan'
@@ -120,7 +91,7 @@ sensors_layout_names_df = pd.read_csv(sensors_layout_sheet)
 sub_IDs = []
 psd_all_sens_all_subs = []
 
-for i, subjectID in enumerate(good_subject_pd.head(5).index):
+for i, subjectID in enumerate(good_subject_pd.index):
     # Read subjects one by one and calculate lateralisation index for each pair of sensor and all freqs
     epoched_fname = 'sub-CC' + str(subjectID) + '_ses-rest_task-rest_megtransdef_epo.fif'
     epoched_fif = op.join(epoched_dir, epoched_fname)
@@ -148,7 +119,7 @@ psd_all_sens_all_subs_arr = np.array(psd_all_sens_all_subs)  # shape: #subjects,
 # Calculate grand average PSD for each channel
 grand_average_psd = np.mean(psd_all_sens_all_subs_arr, axis=0)  # shape: #channels, #freqs
 
-# Filter out frequencies below 5 Hz - to show low amplitudes in high freqs better
+# Filter out frequencies below 60 Hz - to show low amplitudes in high freqs better
 min_freq = 60
 freq_mask = np.where(freqs > min_freq)[0]
 filtered_freqs = freqs[freq_mask]
@@ -160,8 +131,8 @@ all_left_names = [f'{row["left_sensors"][0:8]}' for _, row in sensors_layout_nam
 right_epochs = epochs.copy().pick(all_right_names)
 left_epochs = epochs.copy().pick(all_left_names)
 
-right_ch_indices = [right_epochs.info['ch_names'].index(ch) for ch in sensors_layout_names_df['right_sensors']]
-left_ch_indices = [left_epochs.info['ch_names'].index(ch) for ch in sensors_layout_names_df['left_sensors']]
+right_ch_indices = [epochs.info['ch_names'].index(ch) for ch in sensors_layout_names_df['right_sensors']]
+left_ch_indices = [epochs.info['ch_names'].index(ch) for ch in sensors_layout_names_df['left_sensors']]
 
 grand_average_psd_right = grand_average_psd[right_ch_indices][:, freq_mask]
 grand_average_psd_left = grand_average_psd[left_ch_indices][:, freq_mask]
@@ -169,8 +140,17 @@ grand_average_psd_left = grand_average_psd[left_ch_indices][:, freq_mask]
 evoked_right = mne.EvokedArray(grand_average_psd_right, right_epochs.info, tmin=0, comment=f'right sensors')
 evoked_left = mne.EvokedArray(grand_average_psd_left, right_epochs.info, tmin=0, comment=f'left sensors')
 
+# Replace time of evoked objects with freqs to have x-axis in frequency range
+"""still needs work, times has no setter in evoked array
+evoked_right = evoked_right.crop(tmin=filtered_freqs[0], tmax=None)
+evoked_left = evoked_left.crop(tmin=filtered_freqs[0], tmax=filtered_freqs[-1])
+
+evoked_right.times = filtered_freqs
+evoked_left.times = filtered_freqs
+"""
+
 # Create a figure for the topomap plot
-fig, ax = plt.subplots(1, 1, figsize=(12, 6))
+grand_avg_fig, ax = plt.subplots(1, 1, figsize=(12, 6))
 
 # Plot the right sensors' evoked responses
 evoked_right.plot_topo(axes=ax, color='orange', show=False, legend=False)
@@ -186,36 +166,10 @@ plt.legend(handles, labels, loc='upper right')
 
 # Adding title and adjusting plot properties
 plt.title('Grand Average PSD Right vs Left Sensors')
-
-
-# Remove grid lines
 ax.grid(True)
-
-# Show the plot
 plt.show()
 
-
-
-# Create Evoked objects for right sensors and 
-right_ch_indices = []
-left_ch_indices = []
-
-# Create an EvokedArray object from the DataFrame
-rightraw = raw.copy().pick(all_right_names)
-evoked = mne.EvokedArray(correlation_df.values.T, rightraw.info, tmin=0, comment=f'spearmanr')
-
-# Plot the correlation values with similar format as plot_topo
-evoked_fig_output_fname = op.join(op.join(jenseno_dir, 'Projects/subcortical-structures/resting-state/results/CamCan/Results/correlation_plot_topos/subtraction-nonoise', f'{substr}.png'))
-evoked_fig = evoked.plot_topo(title=f"correlation between frequency and {substr} laterality")
-evoked_fig.savefig(evoked_fig_output_fname)
-
-for r_ch, l_ch in zip(sensors_layout_names_df['right_sensors'], sensors_layout_names_df['left_sensors']):
-    right_ch_idx = right_epochs.info['ch_names'].index(r_ch)
-    right_ch_indices.append(right_ch_idx)
-
-    left_ch_idx = left_epochs.info['ch_names'].index(l_ch)
-    left_ch_indices.append(left_ch_idx)
-# Set x-axis label and limits
-ax.set_xlabel('Frequency (Hz)')
-ax.set_xlim([filtered_freqs[0], filtered_freqs[-1]])
-ax.set_ylim([np.min(grand_average_psd_right), np.max(grand_average_psd_right)])  # Adjust the y-axis limits if needed
+# Save
+grand_avg_fig_output_fname = op.join(op.join(jenseno_dir, 'Projects/subcortical-structures/resting-state/results/CamCan/Results/test_plots', 'grand_average_plot_topo_60.tiff')
+                                     )
+grand_avg_fig.savefig(grand_avg_fig_output_fname, dpi=1500)
