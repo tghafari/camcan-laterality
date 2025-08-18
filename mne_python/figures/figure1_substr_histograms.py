86c2d8dca5c736660d52a5f645adf21fd6ca340f
tghafari <tara.ghafari@gmail.com>
2025-08-18 19:22:20 +0100

    working on figure1 codes

--- mne_python/figures/figure1_substr_histograms.py
+++ mne_python/figures/figure1_substr_histograms.py
@@ -4,6 +4,7 @@
 
 
 import os.path as op
+import os
 
 import numpy as np
 import pandas as pd
@@ -16,28 +17,27 @@ from scipy.stats import shapiro
 platform = 'mac'  # 'bluebear' or 'mac'?
 
 if platform == 'bluebear':
-    rds_dir = '/rds/projects/q/quinna-camcan'
+    quinna_dir = '/rds/projects/q/quinna-camcan'
     jenseno_dir = '/rds/projects/j/jenseno-avtemporal-attention'
     sub2ctx_dir = '/rds/projects/j/jenseno-sub2ctx/camcan'
 elif platform == 'mac':
-    rds_dir = '/Volumes/quinna-camcan'
+    quinna_dir = '/Volumes/quinna-camcan'
     jenseno_dir = '/Volumes/jenseno-avtemporal-attention'
     sub2ctx_dir = '/Volumes/jenseno-sub2ctx/camcan'
 
-epoched_dir = op.join(rds_dir, 'derivatives/meg/sensor/epoched-7min50')
-info_dir = op.join(rds_dir, 'dataman/data_information')
+epoched_dir = op.join(quinna_dir, 'derivatives/meg/sensor/epoched-7min50')
+info_dir = op.join(quinna_dir, 'dataman/data_information')
 good_sub_sheet = op.join(info_dir, 'demographics_goodPreproc_subjects.csv')  # list of all subjects at the beginning
 final_sub_list_path = op.join(info_dir, 'FINAL_sublist-vol-psd-outliers-removed.csv')  # list of subjects after removing size below 10th percentile and power nonoise_subtraction_abs_thresh
 
 volume_sheet_dir = 'derivatives/mri/lateralized_index'
+sub_list= op.join(quinna_dir, 'dataman/data_information/dblCheck_last_FINAL_sublist-vol-outliers-removed.csv'),
 substr_vol_sheet_fname = op.join(sub2ctx_dir, volume_sheet_dir, 'all_subs_substr_volumes.csv')
 lat_sheet_fname = op.join(sub2ctx_dir, volume_sheet_dir, 'lateralization_volumes.csv')  # this is lateralisation vol for all subjects that will be filtered to exclude vol outliers in this script
 lat_sheet_fname_nooutlier = op.join(sub2ctx_dir, volume_sheet_dir, 'lateralization_volumes_no-vol-outliers.csv')  # only volume outlier
 outlier_subjectID_vol_fname = op.join(info_dir, 'outlier_subjectID_vol.csv')  # this only contains vol outliers
 subject_list_no_vol_outliers = op.join(info_dir, 'FINAL_sublist-vol-outliers-removed.csv')  # in case we only wanted to exclude vol outliers
-output_plot_dir = op.join(jenseno_dir, 'Projects/subcortical-structures/resting-state/results/CamCan/Results/volume-plots')
-# outlier_subjectID_psd_csv = op.join(info_dir, 'outlier_subjectID_psd_df.csv')  # don't remember how the outlier was calculated for psd here
-# lat_sheet_fname_final_subs = op.join(sub2ctx_dir, volume_sheet_dir, 'lateralization_volumes_final_subs.csv')  # this is not used with old final list of subs
+fig_output_root= '/Users/t.ghafari@bham.ac.uk/Library/CloudStorage/OneDrive-UniversityofBirmingham/Desktop/BEAR_outage/CamCAN-results/Manuscript/Figures',
 
 # Define colormap and structures
 colormap = ['#FFD700', '#8A2BE2', '#191970', '#8B0000', '#6B8E23', '#4B0082', '#ADD8E6']
@@ -60,21 +60,26 @@ def plot_lateralisation_volumes(substr_lat_df, structures, colormap):
     each subcortical structure.
     """
 
+    def ensure_dir(path):
+        os.makedirs(path, exist_ok=True)
+
+    def save_figure_all_formats(fig, out_dir, basename, dpi=800):
+        ensure_dir(out_dir)
+        base = basename.replace(' ', '_')
+        fig.savefig(op.join(out_dir, f"{base}.tiff"), dpi=dpi, format='tiff', bbox_inches='tight')
+        fig.savefig(op.join(out_dir, f"{base}.png"),  dpi=dpi, format='png',  bbox_inches='tight')
+        fig.savefig(op.join(out_dir, f"{base}.svg"), format='svg', bbox_inches='tight')
+
     # Extract lateralization volumes
     lateralization_volume = substr_lat_df.iloc[:,1:8].to_numpy()
 
     # null hypothesis (H0) mean value
-    throw_out_outliers = False
     null_hypothesis_mean = 0.0
     p_vals = []  # is used for both wilcox and ttest
 
     # wilcoxon p-vals
     null_hypothesis_median = 0.0
 
-    # Perform the normality test analytically and graphically:
-    """ p > 0.05 is normal distribution""" 
-    p_values_shapiro = test_normality(structures, lateralization_volume)
-        
     # Plotting histograms
     n_structures = len(structures)
     n_cols = 4
@@ -95,23 +100,10 @@ def plot_lateralisation_volumes(substr_lat_df, structures, colormap):
         valid_lateralization_volume = lateralization_volume[:, idx]
         lateralization_volume_hist = np.histogram(valid_lateralization_volume, bins=10, density=False)
 
-        # Throw out the outliers
-        if throw_out_outliers:
-                mean_lateralization_volume = np.nanmean(valid_lateralization_volume)
-                std_lateralization_volume = np.nanstd(valid_lateralization_volume)
-                threshold = mean_lateralization_volume - (2.5 * std_lateralization_volume)
-                valid_lateralization_volume[:][valid_lateralization_volume[:] <= threshold] = np.nan
-        
-        if p_values_shapiro[idx] > 0.05:
-            # 1 sample t-test for left/right lateralisation (for normal distributions)
-            _, t_p_value = stats.ttest_1samp(valid_lateralization_volume, null_hypothesis_mean)
-            p_vals.append(t_p_value)
-
-        elif p_values_shapiro[idx] < 0.05:
-            # one sample wilcoxon signed rank (for non normal distributions)
-            _, wilcox_p = stats.wilcoxon(valid_lateralization_volume - null_hypothesis_median,
-                                            zero_method='wilcox', correction=False)
-            p_vals.append(wilcox_p)
+        # one sample wilcoxon signed rank (for non normal distributions)
+        _, wilcox_p = stats.wilcoxon(valid_lateralization_volume - null_hypothesis_median,
+                                        zero_method='wilcox', correction=False)
+        p_vals.append(wilcox_p)
 
         # Plot histogram with colors
         x = lateralization_volume_hist[1]
@@ -139,4 +131,7 @@ def plot_lateralisation_volumes(substr_lat_df, structures, colormap):
         fig.delaxes(axs[i])
 
     plt.tight_layout()
-    plt.savefig(op.join(output_plot_dir, 'lateralisation-histograms-final_subs_no-vol-outliers_nonormalcurve.png'), dpi=3000)
+
+    basename = 'lateralisation-histograms-final_subs_no-vol-outliers_nonormalcurve'
+    save_figure_all_formats(fig, fig_output_root, basename)
+                            
